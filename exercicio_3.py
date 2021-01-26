5ac30adbe3ccb6068638cf123215767090d6b2da
Leonardo Oliveira <leonardo-rener@hotmail.com>
2021-01-26 16:24:06 -0300

    Aula 03
    Adicionada a terceira aula do curso de python - Tema: Dicionarios

---
+++ exercicio_3.py
@@ -0,0 +1,11 @@
+### Implemente aqui a função ###
+
+        
+# Função main - Não alterar! #
+if __name__ == "__main__":
+    Nome = ["Ana Claudia", "Leonardo", "Carol", "Jaqueline"]
+    Curso = ["Eng. Eletrica", "Eng. Computacao", "Psicologia", "Pintura"]
+    RA    = [231057, 201270, 152731, 241204]
+
+    ### Implemente aqui a chamada da funcao e o for no formato "O ... é ..." para a impressao ###
+
